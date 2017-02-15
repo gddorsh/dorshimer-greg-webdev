5a3addc984c1a24f0208486e53b435f82ae141c8
gddorsh <dorshimer.g@husky.neu.edu>
2017-02-14 23:27:40 -0500

    did page edit event handlers

--- public/assignment/views/page/controllers/page-edit.controller.client.js
+++ public/assignment/views/page/controllers/page-edit.controller.client.js
@@ -3,12 +3,28 @@
         .module("WebAppMaker")
         .controller("PageEditController", PageEditController);
 
-    function PageEditController($routeParams, PageService) {
+    function PageEditController($routeParams, $location, PageService) {
         var vm = this;
+        var userId = $routeParams['uid'];
+        vm.userId = userId;
+        var websiteId = $routeParams['wid'];
+        vm.websiteId = websiteId;
         var pageId = $routeParams['pid'];
+        vm.pageId = pageId;
+        vm.update = update;
         function init() {
             vm.page = PageService.findPageById(pageId);
         }
         init();
+
+        function update(page) {
+            var newPage = PageService.updatePage(page._id, page);
+            if (newPage == null) {
+                console.log('failed to update page');
+            } else {
+                console.log('successfully updated page');
+                $location.url('#/user/' + userId + "/website/" + websiteId + '/page');
+            }
+        }
     }
 })();
