88240d514108c00029d4f1dab0c30df0196ba162
gddorsh <dorshimer.g@husky.neu.edu>
2017-02-15 12:32:08 -0500

    did event handlers for page

--- public/assignment/views/page/controllers/page-list.controller.client.js
+++ public/assignment/views/page/controllers/page-list.controller.client.js
@@ -5,10 +5,14 @@
 
     function PageListController($routeParams, PageService) {
         var vm = this;
+        var userId = $routeParams['uid'];
+        vm.userId = userId;
         var websiteId = $routeParams['wid'];
+        vm.websiteId = websiteId;
         function init() {
             vm.pages = PageService.findPagesByWebsiteId(websiteId);
         }
         init();
     }
+
 })();
--- public/assignment/views/page/controllers/page-new.controller.client.js
+++ public/assignment/views/page/controllers/page-new.controller.client.js
@@ -3,11 +3,27 @@
         .module("WebAppMaker")
         .controller("PageNewController", PageNewController)
 
-    function PageNewController() {
+    function PageNewController($routeParams, PageService) {
         var vm = this;
-        // TODO
-    }
+        var userId = $routeParams['uid'];
+        vm.userId = userId;
+        var websiteId = $routeParams['wid'];
+        vm.websiteId = websiteId;
+        vm.create = create;
 
-    // TODO
+
+        function create(page) {
+            var id = (new Date()).getTime();
+            vm.page._id = id;
+            vm.page.websiteId = websiteId;
+            var newPage = PageService.createPage(page);
+            if (newPage) {
+                console.log("new page created successfully");
+            } else {
+                console.log("failed to create new page");
+            }
+
+        }
+    }
 
 })();
