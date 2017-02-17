55c5591492bba49751cd7980d7a56c9f0e866ec6
gddorsh <dorshimer.g@husky.neu.edu>
2017-02-17 15:22:18 -0500

    fix

--- public/assignment/services/widget.service.client.js
+++ public/assignment/services/widget.service.client.js
@@ -9,7 +9,7 @@
             { "_id": "123", "widgetType": "HEADER", "pageId": "321", "size": 2, "text": "GIZMODO" },
             { "_id": "234", "widgetType": "HEADER", "pageId": "321", "size": 4, "text": "Lorem ipsum" },
             { "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%",
-                "url": "http://i.imgur.com/gRDmTYkr.jpg" },
+                "url": "https://i.imgur.com/gRDmTYkr.jpg" },
             { "_id": "456", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem Ipsum</p>" },
             { "_id": "567", "widgetType": "HEADER", "pageId": "321", "size": 4, "text": "Lorem ipsum" },
             { "_id": "678", "widgetType": "YOUTUBE", "pageId": "321", "width": "100%",
